1674a86171c7dcb59ea5226cf112bbd86a01015e
Kachel Wittig <kjwittig@gmail.com>
2018-03-22 20:50:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(string)
 end
 
 def words_starting_with_un_and_ending_with_ing(string)
-  string.scan /bun\w*ing\b/
+  string.scan /\bun\w*ing\b/
 end
 
 def words_five_letters_long(string)
