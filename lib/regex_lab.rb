49ee2364876ba4cec5aeb0075f3bc22966a25afe
Kachel Wittig <kjwittig@gmail.com>
2018-03-22 20:49:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(string)
 end
 
 def words_starting_with_un_and_ending_with_ing(string)
-  string.scan /b(un)\w(ing)\b/
+  string.scan /bun\wing\b/
 end
 
 def words_five_letters_long(string)
