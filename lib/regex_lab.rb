d8a94b6bc89d64c3eb551173765f11006dc30ddc
Kachel Wittig <kjwittig@gmail.com>
2018-03-22 21:37:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,5 +21,9 @@ def first_word_capitalized_and_ends_with_punctuation?(string)
 end
 
 def valid_phone_number(string)
-  string.match /(\d{3}[- ]?|\(\d{3}\) )\d{3}[- ]?\d{4}/
+  if string.match /(\d{3}[- ]?|\(\d{3}\) )\d{3}[- ]?\d{4}/
+    return true
+  else
+    return false
+end
 end
