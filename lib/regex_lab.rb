a49dcc4e531c6496a17def0cba10654636853bd8
Kachel Wittig <kjwittig@gmail.com>
2018-03-22 20:48:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,18 @@
-def starts_with_a_vowel?(word)
-
+def starts_with_a_vowel?(string)
+  !!(string =~ /\b[aeiou]/i)
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-
+def words_starting_with_un_and_ending_with_ing(string)
+  string.scan /b(un)\w(ing)\b/
 end
 
-def words_five_letters_long(text)
-
+def words_five_letters_long(string)
+  string.scan /\b\w{5}\b/
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
+def first_word_capitalized_and_ends_with_punctuation?(string)
 end
 
-def valid_phone_number?(phone)
-
+def valid_phone_number(string)
+  string =~ /(\d{10}/
 end
