2a6d85b4e439826bde831bb00ec2fe84c8abfdbc
Kachel Wittig <kjwittig@gmail.com>
2018-03-22 21:39:32 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
-def starts_with_a_vowel?(word)
+require "pry"
 
+def starts_with_a_vowel?(string)
+  !!(string =~ /\b[aeiou]/i)
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-
+def words_starting_with_un_and_ending_with_ing(string)
+  string.scan /\bun\w*ing\b/
 end
 
-def words_five_letters_long(text)
-
+def words_five_letters_long(string)
+  string.scan /\b\w{5}\b/
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
+def first_word_capitalized_and_ends_with_punctuation?(string)
+  if string.match /\A[A-Z].*[.,;?!]\z/
+    return true
+  else
+    false
+  end
 end
 
-def valid_phone_number?(phone)
-
+def valid_phone_number?(string)
+  if string.match /(\d{3}[- ]?|\(\d{3}\) ?)\d{3}[- ]?\d{4}/
+    return true
+  else
+    return false
+  end
 end
