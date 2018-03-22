20752eb39cff16bef18a3f5b3fd1fdcf631e6eb6
Kachel Wittig <kjwittig@gmail.com>
2018-03-22 20:59:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,8 +10,9 @@ def words_five_letters_long(string)
   string.scan /\b\w{5}\b/
 end
 
-# def first_word_capitalized_and_ends_with_punctuation?(string)
-# end
+def first_word_capitalized_and_ends_with_punctuation?(string)
+  !!string.scan =~ /\b[A-Z]\w*\b\S/
+end
 #
 # def valid_phone_number(string)
 #   string =~ /(\d{10}/
